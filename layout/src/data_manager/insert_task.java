f3ff54848ca32e56088f4ee944d0f51bf95e1b8c
eviltwin94 <gabr1el@live.com.pt>
2017-04-18 23:55:28 +0100

    DB alterations

---
+++ layout/src/data_manager/insert_task.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data_manager;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author gabriel
+ */
+public class insert_task {
+    
+    /**
+     * Connect to the test.db database
+     *
+     * @return the Connection object
+     */
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:DB_RFMA/RFMA_DB.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+ 
+    /**
+     * Insert a new row into the warehouses table
+     *
+     * @param name
+     * @param capacity
+     */
+    public void insert(String name, double capacity) {
+        String sql = "INSERT INTO warehouses(name,capacity) VALUES(?,?)";
+ 
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setDouble(2, capacity);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ 
+        insert_task app = new insert_task();
+        
+        app.insert("Raw Materials", 3000);
+       
+    }
+ 
+}
+    
+
