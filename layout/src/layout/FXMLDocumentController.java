12aa226555f135d6c3e04b120da291afd0d7cc02
eviltwin94 <gabr1el@live.com.pt>
2017-05-04 10:20:11 +0100

    O eugénio é panisca

--- layout/src/layout/FXMLDocumentController.java
+++ layout/src/layout/FXMLDocumentController.java
@@ -8,13 +8,20 @@ package layout;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import static javafx.scene.input.KeyCode.T;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  *
@@ -49,7 +56,14 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+         
+      
+      
+     
+    
+        
+        
     }    
 
     @FXML
