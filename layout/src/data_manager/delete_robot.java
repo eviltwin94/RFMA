ca9dc66fba482ff779d436677e4e979023d22e7b
eviltwin94 <gabr1el@live.com.pt>
2017-05-09 18:03:27 +0100

    já não me lembro de onde fiquei

--- layout/src/data_manager/delete_robot.java
+++ layout/src/data_manager/delete_robot.java
@@ -5,10 +5,47 @@
  */
 package data_manager;
 
+//O porque desta classe estar aqui é um mistério, não me lembro do que queria fazer. Vou manter para o caso de ser necessária posteriormente
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author gabriel
  */
 public class delete_robot {
+    
+     private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:DB_RFMA/RFMA_DB.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+     
+     
+     
+  public void delete(int id) {
+        String sql = "DELETE FROM Robot WHERE robot_id = ?";
+ 
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+ 
+            // set the corresponding param
+            pstmt.setInt(1, id);
+            // execute the delete statement
+            pstmt.executeUpdate();
+ 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     
 }
