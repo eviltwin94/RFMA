68b727e565f3b854a4adb867788564210fe76732
eviltwin94 <gabr1el@live.com.pt>
2017-05-11 21:15:30 +0100

    mensagens a funcionar

--- layout/src/data_manager/update_operation_data.java
+++ layout/src/data_manager/update_operation_data.java
@@ -5,10 +5,14 @@
  */
 package data_manager;
 
+import static java.lang.Math.sqrt;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DecimalFormat;
 
 /**
  *
@@ -32,12 +36,13 @@ public class update_operation_data {
     
 
 
-public void update( double operation_time, double total_distance, double total_energy_consumption, int charges_number, int discharges_number, String name, int type) {
+public void update( double operation_time, double total_distance, double total_energy_consumption, double timestamp,  String name, int type, double x, double y) {
         String sql = "UPDATE Operation SET operation_time = ?,  "
                  + "total_distance = ?, "
                  + "total_energy_consumption = ?, "
-                + "charges_number = ?, "
-                + "discharges_number = ? "
+                + "oldTimestamp = ?, "        
+                + "x = ?, "
+                + "y = ? "
                 
                 + "WHERE name = ?";
                 
@@ -49,9 +54,12 @@ try (Connection conn = this.connect();
             pstmt.setDouble(1,operation_time);
             pstmt.setDouble(2, total_distance);
             pstmt.setDouble(3, total_energy_consumption);
-            pstmt.setInt(4, charges_number);
-            pstmt.setInt(5, discharges_number);
-            pstmt.setString(6, name);
+            pstmt.setDouble(4, timestamp);
+            //pstmt.setInt(5, discharges_number);
+                        pstmt.setDouble(5,x);
+            pstmt.setDouble(6,y);
+
+            pstmt.setString(7, name);
             //pstmt.setInt(6, id_operation);
             //pstmt.setInt(8, type);
             
@@ -68,13 +76,445 @@ try (Connection conn = this.connect();
     
 }
     
- public static void main(String[] args) {
+
+
+public double fetch_operation_time(String name){
+
+String sql = "SELECT operation_time, name FROM Operation";
         
-        update_operation_data app = new update_operation_data();
-        // update the warehouse with id 3
-        app.update( 1, 1,1,1,1, "Pioneer_P3DX_nr4", 3);
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                
+                String aux = rs.getString("name");
+                
+                if(aux.equals(name)){
+                    
+                    double temp = rs.getDouble("operation_time");
+                    System.out.println("teste de validação");
+                    
+                return(temp);
+                }
+                
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return(-1);
+}
+  
+
+
+
+public double fetch_consumed_power(String name){
+
+String sql = "SELECT total_energy_consumption, name FROM Operation";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                
+                String aux = rs.getString("name");
+                
+                if(aux.equals(name)){
+                    
+                    double temp = rs.getDouble("total_energy_consumption");
+                    
+                    
+                return(temp);
+                }
+                
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return(-1);
+
+
+}
+
+public double consumed_power(double a, double b, double c, double v, double w){
+
+double temp = a + b*(sqrt(v*v)) + c*(sqrt(w*w));
+
+return(temp);
+}
+
+
+public double fetch_a(int type){
+
+String sql = "SELECT a, robot_type_id FROM RobotType";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                
+                int aux = rs.getInt("robot_type_id");
+                
+                if(aux == type){
+                    
+                    double temp = rs.getDouble("a");
+                    
+                    
+                return(temp);
+                }
+                
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return(-1);
+}
+
+public double fetch_b(int type){
+
+String sql = "SELECT b, robot_type_id FROM RobotType";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                
+                int aux = rs.getInt("robot_type_id");
+                
+                if(aux == type){
+                    
+                    double temp = rs.getDouble("b");
+                    
+                    
+                return(temp);
+                }
+                
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return(-1);
+}
+
+public double fetch_c(int type){
+
+String sql = "SELECT c, robot_type_id FROM RobotType";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                
+                int aux = rs.getInt("robot_type_id");
+                
+                if(aux == type){
+                    
+                    double temp = rs.getDouble("c");
+                    
+                    
+                return(temp);
+                }
+                
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return(-1);
+}
+
+public double fetch_d(int type){
+
+String sql = "SELECT d, robot_type_id FROM RobotType";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                
+                int aux = rs.getInt("robot_type_id");
+                
+                if(aux == type){
+                    
+                    double temp = rs.getDouble("d");
+                    
+                    
+                return(temp);
+                }
+                
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return(-1);
+}
+
+public double distance(double x1, double x2, double y1, double y2){
+
+    double x = x2-x1;
+    double y = y2 - y1;
+    
+    double d = sqrt(x*x + y*y);
+    
+    return(d);
+
+}
+
+public double fetch_x(String name){
+
+String sql = "SELECT x, name FROM Operation";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                
+                String aux = rs.getString("name");
+                
+                if(aux.equals(name)){
+                    
+                    double temp = rs.getDouble("x");
+                    
+                    
+                return(temp);
+                }
+                
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return(-1);
+
+}
+
+public double fetch_y(String name){
+
+String sql = "SELECT y, name FROM Operation";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                
+                String aux = rs.getString("name");
+                
+                if(aux.equals(name)){
+                    
+                    double temp = rs.getDouble("y");
+                    
+                    
+                return(temp);
+                }
+                
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return(-1);
+
+}
+
+public void update_charge(String name, double charge) {
+        String sql = "UPDATE Robot SET charge = ?  "
+               
+                
+                
+                + "WHERE robot_name = ?";
+                
+
+try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+ 
+            // set the corresponding param
+            pstmt.setDouble(1, charge);
+            pstmt.setString(2, name);
+            
+            
+            
+            
+            
+            
+            
+            // update 
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    
+}
+
+
+public double calc_charge(double charge, double capacity, double p, double t){
+
+    //potência x tempo em horas * conversão para percentagem
+double aux = p * (t/3600)*(100/capacity);
+
+aux = charge - aux;
+
+return(aux);
+
+}
+
+public double fetch_charge(String name){
+
+
+String sql = "SELECT charge, robot_name FROM Robot";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                
+                String aux = rs.getString("robot_name");
+                
+                if(aux.equals(name)){
+                    
+                    double temp = rs.getDouble("charge");
+                    
+                    
+                return(temp);
+                }
+                
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return(-1);
+
+
+}
+
+public double fetch_capacity(String name){
+String sql = "SELECT capacity, robot_name FROM Robot";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                
+                String aux = rs.getString("robot_name");
+                
+                if(aux.equals(name)){
+                    
+                    double temp = rs.getDouble("capacity");
+                    
+                    
+                return(temp);
+                }
+                
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return(-1);
+
+
+}
+
+public double fetch_oldTimestamp(String name){
+    
+    
+String sql = "SELECT oldTimestamp, name FROM Operation";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                
+                String aux = rs.getString("name");
+                
+                if(aux.equals(name)){
+                    
+                    double temp = rs.getDouble("oldTimestamp");
+                    
+                    
+                return(temp);
+                }
+                
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return(-1);
+
+}
+
+public void update_xy(String name, double x, double y) {
+        String sql = "UPDATE Operation SET x = ?,  "
+               + "y = ? "
+                
+                
+                + "WHERE name = ?";
+                
+
+try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+ 
+            // set the corresponding param
+            pstmt.setDouble(1, x);
+            pstmt.setDouble(2, y);
+            pstmt.setString(3, name);
+            
+            
+            
+            
+            
+            
+            // update 
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    
+}
+
+
+
+
+
+public static void main(String[] args) {
+    update_operation_data app = new update_operation_data();
+
+    
+    
+    double x = app.distance(0,0,0,0);
+    
+ System.out.println(x);
+
+    
     }
 
 
-    
+
 }
--- layout/src/server/RequestHandler.java
+++ layout/src/server/RequestHandler.java
@@ -7,104 +7,162 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import static java.lang.Math.sqrt;
 
 import java.net.ServerSocket;
 import java.net.Socket;
 import system_manager.OperationData;
 
 public class RequestHandler implements Runnable {
-  private final Socket client;
-  ServerSocket serverSocket = null;
+
+    private final Socket client;
+    ServerSocket serverSocket = null;
     public String nome = "vazio";
+    public int tipo = 0;
 
     public RequestHandler(Socket client) {
-      this.client = client;
+        this.client = client;
     }
 
     @Override
     public void run() {
-      try ( BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-	/*BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));*/) {
-	System.out.println("Thread started with name:" + Thread.currentThread().getName());
-	String robotInput;
-	while ((robotInput = in.readLine()) != null) {
-          
-          String retval[] = new String[4];
-          int msgType = Character.getNumericValue(robotInput.charAt(0));
-          if (msgType == 1) { // 1:<timestamp>;<robot_type>;<robot_name>
-              
-              retval = (robotInput.substring(2)).split(";");
-              int timestamp = Integer.parseInt(retval[0]);
-              int robotType = Integer.parseInt(retval[1]);
-              String robotName = retval[2];
-              System.out.println("From " + Thread.currentThread().getName() + ": msgType=" + msgType);
-              System.out.println("                                   timestamp = " + timestamp);
-              System.out.println("                                   robotType = " + robotType);
-              System.out.println("                                   robotName = " + robotName);
-              
-              /*
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream())); /*BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));*/) {
+            System.out.println("Thread started with name:" + Thread.currentThread().getName());
+            String robotInput;
+            while ((robotInput = in.readLine()) != null) {
+
+                String retval[] = new String[4];
+                int msgType = Character.getNumericValue(robotInput.charAt(0));
+                if (msgType == 1) { // 1:<timestamp>;<robot_type>;<robot_name>
+
+                    retval = (robotInput.substring(2)).split(";");
+                    int timestamp = Integer.parseInt(retval[0]);
+                    int robotType = Integer.parseInt(retval[1]);
+                    String robotName = retval[2];
+                    System.out.println("From " + Thread.currentThread().getName() + ": msgType=" + msgType);
+                    System.out.println("                                   timestamp = " + timestamp);
+                    System.out.println("                                   robotType = " + robotType);
+                    System.out.println("                                   robotName = " + robotName);
+
+                    /*
               OperationData aux = new OperationData();
               
               aux.name = robotName;
               aux.type = robotType;
-*/
-               nome = getName(robotName);
-              
-              insert_operation_data app = new insert_operation_data();
-              
-              if(!(app.verify_existence(nome))){
-              
-              app.insert(robotName, robotType);
-              
-              }
-           
+                     */
+                    nome = getName(robotName);
+                    tipo = getRobotType(robotType);
 
-              
-          } else if (msgType == 2) {  // 2:<timestamp>;<task_type>;<x>,<y>,<theta>;<v>,<w>
-              retval = (robotInput.substring(2)).split(";");
-              int timestamp = Integer.parseInt(retval[0]);
-              int taskType = Integer.parseInt(retval[1]);
-              String robotPos = retval[2];
-              String robotVel = retval[3];
-              retval = robotPos.split(",");
-              double x = Double.parseDouble(retval[0]);
-              double y = Double.parseDouble(retval[1]);
-              double theta = Double.parseDouble(retval[2]);
-              retval = robotVel.split(",");
-              double v = Double.parseDouble(retval[0]);
-              double w = Double.parseDouble(retval[1]);
-              System.out.println("From " + Thread.currentThread().getName() + ": msgType=" + msgType);
-              System.out.println("                                   timestamp = " + timestamp);
-              System.out.println("                                   robotType = " + taskType);
-              System.out.println("                                   robotPos = " + robotPos);
-              System.out.println("                                   robotVel = " + robotVel);
-              System.out.println("                                   x = " + x + ", y = " + y + ", theta = " + theta);
-              System.out.println("                                   v = " + v + ", w = " + w);
-              System.out.println(nome);
-              
-              update_operation_data app = new update_operation_data();
-              
-              app.update( 1, 1,1,1,1, nome, 3);
-              
-          }
+                    insert_operation_data app = new insert_operation_data();
+update_operation_data set = new update_operation_data();
+                    
+                    set.update_xy(nome, 0,0);
+                    if (!(app.verify_existence(nome))) {
 
-          
-	
-	}
-      } catch (IOException e) {
-           System.out.println("I/O exception: " + e);
+                        app.insert(robotName, robotType);
+
+                    }
+
+                } else if (msgType == 2) {  // 2:<timestamp>;<task_type>;<x>,<y>,<theta>;<v>,<w>
+                    retval = (robotInput.substring(2)).split(";");
+                    int timestamp = Integer.parseInt(retval[0]);
+                    int taskType = Integer.parseInt(retval[1]);
+                    String robotPos = retval[2];
+                    String robotVel = retval[3];
+                    retval = robotPos.split(",");
+                    double x = Double.parseDouble(retval[0]);
+                    //x = sqrt(x*x);
+                    double y = Double.parseDouble(retval[1]);
+                    //y = sqrt(y*y);
+                    double theta = Double.parseDouble(retval[2]);
+                    retval = robotVel.split(",");
+                    double v = Double.parseDouble(retval[0]);
+                    double w = Double.parseDouble(retval[1]);
+                    System.out.println("From " + Thread.currentThread().getName() + ": msgType=" + msgType);
+                    System.out.println("                                   timestamp = " + timestamp);
+                    System.out.println("                                   robotType = " + taskType);
+                    System.out.println("                                   robotPos = " + robotPos);
+                    System.out.println("                                   robotVel = " + robotVel);
+                    System.out.println("                                   x = " + x + ", y = " + y + ", theta = " + theta);
+                    System.out.println("                                   v = " + v + ", w = " + w);
+                    System.out.println(nome);
+
+                    update_operation_data app = new update_operation_data();
+                    //bloco referente ao tempo de operação em segundos
+                    double oper_time = app.fetch_operation_time(nome);
+                    double total_oper_time = oper_time + timestamp / (1000);
+
+                    //bloco referente ao total de energia consumida
+                    double consumo = app.fetch_consumed_power(nome);
+                    double a1 = app.fetch_a(tipo);
+                    double b2 = app.fetch_b(tipo);
+                    double c3 = app.fetch_c(tipo);
+                    
+
+                    double rtconsume = app.consumed_power(a1, b2, c3, v, w);
+
+                    consumo = consumo + rtconsume;
+
+                    //bloco referente a distância
+                    
+                    double x0 = app.fetch_x(nome);
+                    double y0 = app.fetch_y(nome);
+                    
+                    double d = app.distance(x0, x, y0, y);
+                    
+                    
+                    //actualizar a carga
+                    
+                    double carga = app.fetch_charge(nome);
+                    double capacidade = app.fetch_capacity(nome);
+                    double old_timestamp = app.fetch_oldTimestamp(nome);
+                    
+                    if(old_timestamp > timestamp){
+                    
+                        //significa que esta é uma nova sessão
+                        app.calc_charge(carga, capacidade, rtconsume, timestamp);
+                    }else{
+                    
+                    double delta_time = timestamp - old_timestamp;
+                    
+                    app.calc_charge(carga, capacidade, rtconsume, delta_time);
+                    }
+                    
+                  
+                    
+                    //numero de cargas e descargas
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    app.update(total_oper_time, d, consumo, timestamp, nome, tipo, x, y);
+                    
+                
+                }
+
+            }
+        } catch (IOException e) {
+            System.out.println("I/O exception: " + e);
         } catch (Exception ex) {
-	   System.out.println("Exception in Thread Run. Exception : " + ex);
-	  }
+            System.out.println("Exception in Thread Run. Exception : " + ex);
+        }
     }
 
     public String getName(String robotName) {
-    
+
         String nome = robotName;
-        return(nome);
-        
+        return (nome);
+
+    }
+
+    public int getRobotType(int rtype) {
+
+        int type = rtype;
+        return (type);
+
     }
 
 }
-
-
